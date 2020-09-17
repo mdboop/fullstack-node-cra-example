20f93ee2ddd2fcd574d502fa73a448c613e9c988
Matthew Brooks <mdeanbrooks@gmail.com>
2020-09-17 23:58:17 +0900

    Split prod and dev db connection again

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,15 +1,8 @@
-module.exports = {
+const PGConnectionStringParser = require("pg-connection-string")
+
+const baseConnection = {
   type: "postgres",
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  username: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
   synchronize: true,
-  extra: {
-    ssl: true,
-  },
-  logging: false,
   entities: ["src/entity/**/*.ts"],
   migrations: ["src/migration/**/*.ts"],
   subscribers: ["src/subscriber/**/*.ts"],
@@ -19,3 +12,36 @@ module.exports = {
     subscribersDir: "src/subscriber",
   },
 }
+
+let connection
+
+if (process.env.NODE_ENV === "production") {
+  const databaseUrl = process.env.DATABASE_URL
+  const pgConnectionOptions = PGConnectionStringParser.parse(databaseUrl)
+  const productionConnection = {
+    ...baseConnection,
+    name: pgConnectionOptions.name,
+    host: pgConnectionOptions.host,
+    port: pgConnectionOptions.port,
+    username: pgConnectionOptions.username,
+    password: pgConnectionOptions.password,
+    database: pgConnectionOptions.database,
+    extra: {
+      ssl: true,
+    },
+  }
+  connection = productionConnection
+} else {
+  const devConnection = {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    logging: false,
+  }
+
+  connection = devConnection
+}
+
+module.exports = connection
