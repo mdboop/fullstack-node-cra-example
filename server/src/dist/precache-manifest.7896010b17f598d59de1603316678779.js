9ea42537d47af86d4dea4ca94d78499c79b65b52
Matthew Brooks <mdeanbrooks@gmail.com>
2020-09-17 22:14:10 +0900

    Change to dist folder for client build

---
+++ server/src/dist/precache-manifest.7896010b17f598d59de1603316678779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80569137fbd14de59c20a6040e32aef7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc9096583b7b1278bc06",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "61fad6b990be4369a632",
+    "url": "/static/js/2.1387f0b4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1387f0b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc9096583b7b1278bc06",
+    "url": "/static/js/main.f29ebee7.chunk.js"
+  },
+  {
+    "revision": "249ce9805e5f357a809f",
+    "url": "/static/js/runtime-main.c8adf6a6.js"
+  }
+]);
--- server/src/index.ts
+++ server/src/index.ts
@@ -43,7 +43,7 @@ createConnection()
     })
 
     app.get("/*", function (req, res) {
-      res.sendFile(path.join(__dirname, "build", "index.html"))
+      res.sendFile(path.join(__dirname, "dist", "index.html"))
     })
 
     console.log("Established connection to DB.")
